10313f4b75406b57c3353119db8742d0a64a2a6b
paluja <jtp1006@hotmail.com>
2025-02-27 19:46:18 +0100

    si

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pjimenez <pjimenez@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 20:01:30 by pjimenez          #+#    #+#             */
-/*   Updated: 2025/02/25 20:06:05 by pjimenez         ###   ########.fr       */
+/*   Updated: 2025/02/27 19:42:45 by pjimenez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,10 @@ int main()
 {
     ClapTrap r2("R2D2");
     ClapTrap c3("C3PO");
-
+    ClapTrap c4(r2);
+    
+    r2 = c4;
+    
     r2.attack("C3PO");
     c3.takeDamage(5);
     c3.beRepaired(5);
